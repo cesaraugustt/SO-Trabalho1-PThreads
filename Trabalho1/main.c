21ae81e5bf1dde3445791c507eb5756a6e2c334b
César Augusto <cesaraugustobispo@gmail.com>
2025-06-14 16:26:47 -0300

    commit inicial

---
+++ Trabalho1/main.c
@@ -0,0 +1,30 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+
+#pragma comment(lib,"pthreadVC2.lib")
+
+#define HAVE_STRUCT_TIMESPEC
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* threadFunc(void*);
+
+int main(int argc, char* argv[]) {
+	pthread_t thread;
+	if (pthread_create(&thread, NULL, threadFunc, NULL) != 0) {
+		perror("Pthread_create falhou!");
+		exit(1);
+	}
+	if (pthread_join(thread, NULL) != 0) {
+		perror("Pthread_join falhou!");
+		exit(1);
+	}
+	printf("Print do Main");
+	return 0;
+}
+
+void* threadFunc(void* nenhum) {
+	printf("Print da Thread\n");
+}
